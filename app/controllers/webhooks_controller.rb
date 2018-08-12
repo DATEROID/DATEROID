4f8be5043170cf44915b4d801d8b34c936b81076
AkiyoshiOkano <akiyoshiokano0415@gmail.com>
2018-08-12 22:12:00 +0900

    DIALOGFLOWとの連携機能を実装

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,17 +1,22 @@
 class WebhooksController < ApplicationController
+	require 'net/https'
+	require 'uri'
+	require 'json'
+
+	DIALOGFLOW_API_ENDPOINT = "https://api.dialogflow.com/v1/query?v=20150910"
+
     def  fb_bot
-    	# Userモデルを取得
     	p "====[DEBUG param]===="
     	p params
+    	# Userモデルを取得
 
     	# プレーンテキスト取得
-    	message = params["entry"][0]["messaging"][0]
+    	message = "デート相手を探す"
+    	#message = params["entry"][0]["messaging"][0]
 
     	# DialogflowへAPI投げる
-	    	# {Header} Content-Type: application/json; charset=utf-8 Authorization: Bearer ya29.c.Elr1BWZobY_MwExd18D6zwMrPeqIwfuhBVdiW_qlNP6pSCvKqazOZJQHRMHSsnSD9nao9LFzdC2myzSmxVVfz1BHZ_u4MYqDsp8v3LBfvNZt_e4ERR33xZdjtkk
-    		# {body}  {"queryInput": {"text": {"text": "テスト","languageCode": "ja"}},"queryParams": {"timeZone": "Asia/Tokyo"}}
-    		# {URL} https://dialogflow.googleapis.com/v2beta1/projects/dateroid-24d1a/agent/sessions/f98c8502-3cf0-8201-f07a-76c47b49a7e5:detectIntent
-    	action = response.queryResult
+	    dialogflow_result_hash = get_dialogflow_result(DIALOGFLOW_API_ENDPOINT, "デート", 12345)
+	    action = dialogflow_result_hash["result"]["action"] #actionを取得（find_matching, adjust_schedule のようなものを想定）
 
     	# Dialogflowのレスポンスを見てアクション振り分け
     		# Action.allocator(action, mode, user) 的なモジュールを用意すればいいのでは
@@ -21,5 +26,38 @@ class WebhooksController < ApplicationController
     		# 奈良ちゃんがモジュール化してくれる？
     		# Responseモデルとして管理するのがいいと思われる
 
+    	render json: {"test": "test"}
     end
+
+    private
+
+    	def get_dialogflow_result(endpoint, message, session_id)
+    		uri = URI.parse(endpoint)
+
+			header = {
+				'Content-Type': 'application/json;charset=UTF-8',
+				'Authorization': 'Bearer ' + ENV["DIALOGFLOW_CLIENT_ACCESS_TOKEN"]
+			}
+			body = {
+    			contexts: [],
+    			lang: "ja",
+    			query: message,
+    			sessionId: session_id,
+    			timezone: "Japan/Tokyo"
+  			}
+
+			# Create the HTTP objects
+			http = Net::HTTP.new(uri.host, uri.port)
+			request = Net::HTTP::Post.new(uri.request_uri, header)
+			request.body = body.to_json
+
+			http.use_ssl = true
+			http.verify_mode=OpenSSL::SSL::VERIFY_NONE
+
+			# Send the request
+			api_response = http.request(request)
+			result_hash = JSON.parse(api_response.body)
+
+			return result_hash
+		end
 end
