56a02ea10b463d5e73312bf705210ee09a3bb79f
AkiyoshiOkano <akiyoshiokano0415@gmail.com>
2018-08-13 14:23:37 +0900

    各社PF用のWEBHOOKを用意

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   match 'fb_bot_webhook' => 'webhooks#fb_bot', :via => :post
+  match 'line_bot_webhook' => 'webhooks#line_bot', :via => :post
+  match 'google_assisntant_ss_webhook' => 'webhooks#google_assisntant_ss', :via => :post
+  match 'amason_alexa_ss_webhook' => 'webhooks#amason_alexa_ss', :via => :post
+  match 'line_clova_ss_webhook' => 'webhooks#line_clova_ss', :via => :post
 end
